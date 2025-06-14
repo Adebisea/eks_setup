4478c61475d7c63bd6f8cd33f71189cf10eda17c
Adebisea <abdulgkhadijah@gmail.com>
2025-06-14 15:41:13 +0100

    vpc outputs

---
+++ Infra/VPC/output.tf
@@ -0,0 +1,18 @@
+output "pub1_subnet_id" {
+
+    value = aws_subnet.pub1_subnet.id
+}
+
+output "pub2_subnet_id" {
+
+    value = aws_subnet.pub2_subnet.id
+}
+
+output "prv1_subnet_id" {
+
+    value = aws_subnet.prv1_subnet.id
+}
+output "prv2_subnet_id" {
+
+    value = aws_subnet.prv2_subnet.id
+}
