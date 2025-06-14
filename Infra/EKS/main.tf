ec91ddb86b15333cadc9e59095418d447fd9c143
Adebisea <abdulgkhadijah@gmail.com>
2025-06-14 15:44:12 +0100

    eks nodes and roles created

--- Infra/EKS/main.tf
+++ Infra/EKS/main.tf
@@ -41,3 +41,46 @@ resource "aws_eks_cluster" "eks" {
 }
 
 
+#eks worker nodes
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+resource "aws_iam_role_policy_attachment" "node_ecr_PullOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_ecr_ReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  ami_type        = var.ami_type
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = [var.node_instance_type]
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_capacity
+    min_size     = var.min_capacity
+  }
+  depends_on = [aws_iam_role_policy_attachment.eks_worker_policy,
+                aws_iam_role_policy_attachment.node_ecr_PullOnly,
+                aws_iam_role_policy_attachment.node_ecr_ReadOnly
+             ]
+}
