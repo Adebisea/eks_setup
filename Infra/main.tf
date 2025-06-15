8a6f20ed5fdd3733ccce62e245a9fe15221fdad4
Adebisea <abdulgkhadijah@gmail.com>
2025-06-15 16:02:24 +0100

    uncomment other modules to create backend

--- Infra/main.tf
+++ Infra/main.tf
@@ -1,18 +1,28 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
 
-module "vpc" {
-    source = "./VPC"
-}
-module "eks"{
-    source = "./EKS"
-    subnet_ids = [module.vpc.pub1_subnet_id,
-                  module.vpc.pub2_subnet_id,
-                  module.vpc.prv1_subnet_id,
-                  module.vpc.prv2_subnet_id,
-                  module.vpc.prv3_subnet_id]
-}
+
+# terraform {
+#   backend "s3" {
+#     bucket         = "tf-state-techn"
+#     key            = "prod/terraform.tfstate"
+#     region         =  "eu-west-1"
+#  }
+# }
+
+
+# module "vpc" {
+#     source = "./VPC"
+# }
+# module "eks"{
+#     source = "./EKS"
+#     subnet_ids = [module.vpc.pub1_subnet_id,
+#                   module.vpc.pub2_subnet_id,
+#                   module.vpc.prv1_subnet_id,
+#                   module.vpc.prv2_subnet_id,
+#                   module.vpc.prv3_subnet_id]
+# }
 
 
 # create s3 bucket to store terraform state file.
@@ -25,9 +35,3 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-techn"
-    key            = "${var.environment}-terraform.tfstate"
-}
-}
