b47b7a26c0bc5965a66cbac1b6833b4bac9e4308
Adebisea <abdulgkhadijah@gmail.com>
2025-06-16 08:57:30 +0100

    added the vpc and eks module

--- Infra/main.tf
+++ Infra/main.tf
@@ -3,26 +3,32 @@ provider "aws" {
 }
 
 
-# terraform {
-#   backend "s3" {
-#     bucket         = "tf-state-techn"
-#     key            = "prod/terraform.tfstate"
-#     region         =  "eu-west-1"
-#  }
-# }
+terraform {
+  backend "s3" {
+    bucket = "tf-state-techn"
+    key    = "prod/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
 
 
-# module "vpc" {
-#     source = "./VPC"
-# }
-# module "eks"{
-#     source = "./EKS"
-#     subnet_ids = [module.vpc.pub1_subnet_id,
-#                   module.vpc.pub2_subnet_id,
-#                   module.vpc.prv1_subnet_id,
-#                   module.vpc.prv2_subnet_id,
-#                   module.vpc.prv3_subnet_id]
-# }
+module "vpc" {
+  source = "./VPC"
+  prefix = "sharks-${ var.environment}"
+  environment = var.environment
+
+}
+module "eks" {
+  source = "./EKS"
+  vpc_id   = module.vpc.vpc_id
+  k8s_version = "1.32"
+  environment = var.environment
+  subnet_ids = [module.vpc.pub1_subnet_id,
+    module.vpc.pub2_subnet_id,
+    module.vpc.prv1_subnet_id,
+    module.vpc.prv2_subnet_id,
+  module.vpc.prv3_subnet_id]
+}
 
 
 # create s3 bucket to store terraform state file.
