d85f15f383654365cdb39d6497879428f3035fd1
Adebisea <abdulgkhadijah@gmail.com>
2025-06-23 10:35:08 +0100

    use eks default sg and change ami

--- Infra/EKS/main.tf
+++ Infra/EKS/main.tf
@@ -43,35 +43,31 @@ resource "aws_iam_role_policy_attachment" "eks_np_Policy" {
   role       = aws_iam_role.eks_role.name
 }
 
-# EKS Cluster Security Group
-resource "aws_security_group" "eks_cluster_sg" {
-  name        = "${var.cluster_name}-cluster-sg"
-  description = "EKS Cluster Security Group"
-  vpc_id      = var.vpc_id
-
-  # Allow incoming traffic from worker nodes SG
-  ingress {
-    description = "Allow worker nodes to communicate with the cluster API"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    security_groups = [aws_security_group.eks_node_sg.id]
-  }
-
-  # Allow all egress traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.cluster_name}-cluster-sg"
-    environment = var.environment
-  }
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
 }
 
+data "aws_security_group" "eks_cluster_sg" {
+  id = data.aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+}
+
+resource "aws_security_group_rule" "allow_control_plane_to_nodes" {
+  type                     = "ingress"
+  from_port                = 10250
+  to_port                  = 10250
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks_node_sg.id
+  source_security_group_id = data.aws_security_group.eks_cluster_sg.id
+}
+
+resource "aws_security_group_rule" "allow_nodes_to_control_plane" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = data.aws_security_group.eks_cluster_sg.id
+  source_security_group_id = aws_security_group.eks_node_sg.id
+}
 
 # Eks configuration
 resource "aws_eks_cluster" "eks" {
@@ -79,7 +75,7 @@ resource "aws_eks_cluster" "eks" {
   role_arn = aws_iam_role.eks_role.arn
   vpc_config {
     subnet_ids = var.subnet_ids
-    security_group_ids = [aws_security_group.eks_cluster_sg.id]
+    security_group_ids = [aws_security_group.eks_node_sg.id]
   }
   depends_on = [aws_iam_role_policy_attachment.eks_policy,
                 aws_iam_role_policy_attachment.eks_ComputePolicy,
@@ -164,39 +160,19 @@ resource "aws_iam_role_policy_attachment" "node_ecr_ReadOnly" {
   role       = aws_iam_role.eks_node_role.name
 }
 
-# Filtering for Ubuntu Eks ami 
-data "aws_ami_ids" "ubuntu" {
-  owners = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu-eks/k8s_${var.k8s_version}/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-    
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  sort_ascending = true
-
+resource "aws_iam_role_policy_attachment" "node_ssm" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  role       = aws_iam_role.eks_node_role.name
 }
 
-#  Launch template for worker node 
-resource "aws_launch_template" "ubuntu_lt" {
-  name_prefix   = "ubuntu-eks-nodegroup"
-  image_id      = data.aws_ami_ids.ubuntu.ids[0]  
-  instance_type  = var.node_instance_type
-
-  network_interfaces {
-    security_groups = [aws_security_group.eks_node_sg.id]
-  }
+resource "aws_iam_role_policy_attachment" "node_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
 }
-
 
 resource "aws_eks_node_group" "eks_nodes" {
   cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "eks_nodes_group"
   ami_type        = var.ami_type
   node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.subnet_ids 
@@ -205,12 +181,7 @@ resource "aws_eks_node_group" "eks_nodes" {
     max_size     = var.max_capacity
     min_size     = var.min_capacity
   }
-
-  launch_template {
-    id      = aws_launch_template.ubuntu_lt.id
-    version = "$Latest"
-  }
-
+  
   depends_on = [aws_iam_role_policy_attachment.eks_worker_policy,
                 aws_iam_role_policy_attachment.node_ecr_PullOnly,
                 aws_iam_role_policy_attachment.node_ecr_ReadOnly
