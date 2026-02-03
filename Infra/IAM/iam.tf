61b47491b439c04bf45b3b9078ef2db9c67d8b3f
Adebisea <71912632+Adebisea@users.noreply.github.com>
2026-02-03 11:45:51 +0100

    fix oidc url error

--- Infra/IAM/iam.tf
+++ Infra/IAM/iam.tf
@@ -10,7 +10,7 @@ data "tls_certificate" "eks-sharks-cluster" {
 resource "aws_iam_openid_connect_provider" "eks-sharks-cluster" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.example.certificates[0].sha1_fingerprint]
-  url             = aws_eks_cluster.eks.identity[0].oidc[0].issuer
+  url             = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
 
 }
 
